b075fdc1cc8e4999766e973e195300f05f71f777
Gubbsy <32270711+Gubbsy@users.noreply.github.com>
2017-12-14 19:10:45 +0000

    Sprint 6 Fix commit, comenced work on next level system

--- Assets/Scripts/Camera/CameraRig.cs
+++ Assets/Scripts/Camera/CameraRig.cs
@@ -76,13 +76,16 @@ public class CameraRig : MonoBehaviour {
 
         while (!allRendered)
         {
+            
             nextPosition = _camTrans.position - (_levelCenter.transform.position - _camTrans.position).normalized;
 
             for (float j = 0.0f; j < 1.0f; j += Time.deltaTime * 20f) //20f
             {
+                Debug.Log("--> " + j);
                 _camTrans.LookAt(_levelCenter.transform.position);
 
                 _camTrans.position = Vector3.Lerp(previousPos, nextPosition, j);
+                Debug.Log("----> " + _camTrans.position);
                 yield return new WaitForSeconds(0.001f);
             }
             previousPos = _camTrans.position;
@@ -94,6 +97,8 @@ public class CameraRig : MonoBehaviour {
                 if (!_camExtremPoints[x].GetComponent<VisibilityCheck>()._isRendered)
                     allRendered = false;
             }
+            Debug.Log("----> " + allRendered);
+            Debug.Log("----> " + _camExtremPoints.Length);
         }
     }    
 }
